85cc343b7235dc33b51976c2552550435768a1f6
Ankit Jangid <martinxaj@gmail.com>
2023-05-15 19:40:09 +0530

    added link to sidebar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
 
 function Home() {
-  return <div className="bg-[#282a36] h-full text-white">Home</div>;
+  return <div className="bg-[#282a36] h-full text-white">
+    <div className="flex p-9 w-full h-full">
+      <div className='w-1/2 flex items-center justify-center'>hello i am ankit </div>
+      <div className='w-1/2'>hello i am ankit </div>
+      
+    </div>
+  </div>;
 }
 
 export default Home
