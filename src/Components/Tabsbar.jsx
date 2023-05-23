775addcdf68a169102655b030c4abf8caeba091d
Ankit Jangid <martinxaj@gmail.com>
2023-05-23 10:54:05 +0530

    added border to sidebar and taskbar

--- src/Components/Tabsbar.jsx
+++ src/Components/Tabsbar.jsx
@@ -2,30 +2,60 @@ import react_icon from "../assets/react_icon.svg";
 import html_icon from "../assets/html_icon.svg";
 import css_icon from "../assets/css_icon.svg";
 import json_icon from "../assets/json_icon.svg";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+
 function Tabsbar() {
+  let {pathname} = useLocation();
   return (
-    <div className="flex items-center w-full bg-[#21222c] text-sm overflow-y-hidden overflow-x-auto ">
-      <div className="  text-[#ececec]  flex">
-        <Link className="px-5" to="/">
-          <div className="flex px-1 py-3 cursor-pointer gap-1 items-center  justify-center ">
+    <div className="flex items-center w-full bg-[#191a21] text-sm overflow-y-hidden overflow-x-auto ">
+      <div className="  text-[#ececec] gap-0.5 flex">
+        <Link
+          className={`px-5  ${
+            pathname === "/"
+              ? "border-t-4 border-[#bd93f9] bg-[#282a36]"
+              : "bg-[#21222c]"
+          }`}
+          to="/"
+        >
+          <div className="flex px-1 py-3 cursor-pointer gap-1 items-center  justify-center">
             <img src={react_icon} alt="react_icon" className=" w-5" />
             <p className="ml-1">home.jsx</p>
           </div>
         </Link>
-        <Link className="px-5" to="/about">
+        <Link
+          className={`px-5  ${
+            pathname === "/about"
+              ? "border-t-4 border-[#bd93f9] bg-[#282a36]"
+              : "bg-[#21222c]"
+          }`}
+          to="/about"
+        >
           <div className="flex px-1 py-3 cursor-pointer gap-1 items-center justify-center">
             <img src={html_icon} alt="html_icon" className=" w-5" />
             <p className="ml-1">About.html</p>
           </div>
         </Link>
-        <Link className="px-5" to="/contact">
+        <Link
+          className={`px-5  ${
+            pathname === "/contact"
+              ? "border-t-4 border-[#bd93f9] bg-[#282a36]"
+              : "bg-[#21222c]"
+          }`}
+          to="/contact"
+        >
           <div className="flex px-1 py-3 cursor-pointer gap-1 items-center justify-center">
             <img src={css_icon} alt="html_icon" className=" w-5" />
             <p className="ml-1">Contact.css</p>
           </div>
         </Link>
-        <Link className="px-5" to="/project">
+        <Link
+          className={`px-5  ${
+            pathname === "/project"
+              ? "border-t-4 border-[#bd93f9] bg-[#282a36]"
+              : "bg-[#21222c]"
+          }`}
+          to="/project"
+        >
           <div className="flex px-1 py-3 cursor-pointer gap-1 items-center justify-center">
             <img src={json_icon} alt="html_icon" className=" w-5" />
             <p className="ml-1">project.json</p>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
 function Home() {
-  return <div className="bg-[#282a36] h-full text-white">
-    <div className="flex p-9 w-full h-full">
-      <div className='w-1/2 flex items-center justify-center'>hello i am ankit </div>
-      <div className='w-1/2'>hello i am ankit </div>
-      
+  return (
+    <div className="bg-[#282a36] h-full text-white">
+      <div className="flex justify-center  p-9 w-full h-full">
+        <h1 className='font-medium text-2xl'> Welcome to my portfolio! Content coming soon.</h1>
+      </div>
     </div>
-  </div>;
+  );
 }
 
 export default Home
